8c1954f03b5595634682799ac80ab2e062a11052
Goldentoaste <rayg2375@gmail.com>
2021-08-08 02:42:42 -0700

    🤷‍♂️

--- epub2mobi.py
+++ epub2mobi.py
@@ -2,21 +2,24 @@ import os
 import subprocess
 
 
-path = os.getcwd()
-remove_original = True if input("remove original epub?(y/n)") == "y" else False
+if __name__ == "__main__":
+    path = os.getcwd()
+    remove_original = True if input("remove original epub?(y/n)") == "y" else False
 
-for file in os.listdir(path):
-    if file[-5:] == ".epub":
-        print(f"processing file {file}")
-        name = file[: file.find(".epub")]
-        out = subprocess.run(
-            ["ebook-convert", file, f"{name}.mobi", "--dont-compress"],
-            shell=True,
-            capture_output=True,
-        )
-        (
-            (print(f"removing {file}"), os.remove(os.path.join(path, file)))
-            if remove_original
-            else None,
-            print("done."),
-        ) if out.returncode == 0 else print("an error has occured.")
+    for file in os.listdir(path):
+        if file[-5:] == ".epub":
+            print(f"processing file {file}")
+            name = file[: file.find(".epub")]
+            out = subprocess.run(
+                ["ebook-convert", file, f"{name}.mobi", "--dont-compress"],
+                shell=True,
+                capture_output=True,
+            )
+            (
+                (print(f"removing {file}"), os.remove(os.path.join(path, file)))
+                if remove_original
+                else None,
+                print("done."),
+            ) if out.returncode == 0 else print("an error has occured.")
+
+    input("all files processed, press enter to exit :3c")
